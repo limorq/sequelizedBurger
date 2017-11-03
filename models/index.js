2498c34fbabb09b4d089229799b2745712a9d68c
Limor Quintal <limor2004@verizon.net>
2017-11-03 09:54:02 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
@@ -7,13 +6,11 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/..\config\config.json')[env];
 var db        = {};
-
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
@@ -29,8 +26,6 @@ Object.keys(db).forEach(function(modelName) {
     db[modelName].associate(db);
   }
 });
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 module.exports = db;
